b1d02dc07f330d47bda5aca197c071750aadaeed
Fayzaaup <139423061+Fayzaaup@users.noreply.github.com>
2023-11-21 20:28:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function redirectToEmail(subject, body) {
+  const emailLink = `mailto:support@support.whatsapp.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  window.location.href = emailLink;
+}
+
+    // Fungsi untuk mengarahkan ke email 1
+    document.getElementById("emailButton1").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger", "Halo Tim WhatsApp, Saya ingin mengajukan keluhan terkait dengan akun WhatsApp saya yang telah diblokir. Saya mengalami kesulitan karena tidak ada alasan yang jelas mengapa akun saya diblokir. Saya menggunakan akun WhatsApp saya dengan cara yang benar dan tidak pernah melanggar layanan ketentuan WhatsApp. Saya terus menggunakan layanan WhatsApp sebagai alat komunikasi dengan keluarga, teman, dan rekan kerja yang tersebar di Indonesia dan seluruh dunia. Saya sangat bergantung pada aplikasi WhatsApp untuk berkomunikasi sehari-hari dan saya tidak pernah menemukan masalah yang signifikan dengan penggunaannya. Namun, pada satu hari saya merasa bahwa akun WhatsApp saya telah diblokir atau Akun ini tidak diizinkan menggunakan WhatsApp. Saya mulai merasa cemas ketika saya mencoba membuka akun WhatsApp saya dan menemukan bahwa akun saya tidak dapat diakses. Saya mencoba memulihkan akun saya dengan menggunakan nomor ponsel saya, tetapi tetap saja menerima pesan bahwa akun saya telah diblokir. [+62xxx]");
+    });
+
+    // Fungsi untuk mengarahkan ke email 2
+    document.getElementById("emailButton2").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger", "Hallo selamat pagi developer WhatsApp, kenapa nomor WhatsApp saya tiba-tiba terbanned padahal aktivitas WhatsApp messenger saya normal normal saja, mohon unbanned nomor saya [+62xxx] Karna di situ banyak pesan penting dan dokumen penting saya mohon untuk segera di bukakan kembali akun saya, terima kasih.");
+    });
+
+    // Fungsi untuk mengarahkan ke email 3
+    document.getElementById("emailButton3").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger", "Halo admin WhatsApp, saya mohon buka kembali akun WhatsApp saya karena saya tidak melakukan spam, saat ini kemungkinan akun WhatsApp saya di retas oleh orang yang tidak bertanggung jawab dan melakukan pelanggaran saya mohon untuk membuka kembali akun WhatsApp saya agar bisa digunakan karena sangat penting, terimakasih. [+62xx]");
+    });
+    
+    document.getElementById("emailButton4").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","Hallo Admin whatsapp yang saya hormati, akun whatsapp saya tiba-tiba terblokir tanpa alasan, saya dari kemarin belum pernah membuka whatsapp karena saya mengajar anak-anak di sekolah karena mereka ingin melaksanakan ulangan setiap tahunnya dan waktu saya membuka whatsapp saya kembali nomer saya terblokir secara tiba-tiba dan tidak ada alasan yang jelas, saya sangat memohon kepada admin whatsapp untuk menghapus pemblokiran pada akun whatsapp saya karena saya ingin menyampaikan tugas ke murid saya , nomer saya :[+62xx], terimakasih");
+    });
+    
+    document.getElementById("emailButton5").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","hallo mark Zuckerberg saya mendapatkan masalah dan saya juga merasa binggung atas masalah ini saya baru saja membeli sim, terus saya daftarkan nomor itu ke whatsaap, setelah login, tiba-tiba terblokir secara permanen, padahal saya baru saja membeli kartu dan saya mau kasih kabar ke ibu saya, tetapi malah kebanned nomornya nomor : [+62xxx] mohon di buka secepat mungkin, kerena saya ingin mengasih kabar ke keluarga saya, terimakasih");
+    });
+    
+    document.getElementById("emailButton6").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","Eu sou um funcionário privado. E não entendo porque minha conta do WhatsApp pode ser bloqueada sem motivo aparente porque eu realmente preciso da minha conta do WhatsApp... meu número do WhatsApp é [+6283151156627.]");
+    });
+    
+        document.getElementById("emailButton7").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","Hello team support, my number was suddenly banned from WhatsApp and I don't know anything and I have never violated any WhatsApp policy kindly, open my number, myw number is  [+6283151156627]");
+    });
+    
+        document.getElementById("emailButton8").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","Hello parties as well as WhatsApp developers who I respect, I want to ask about my WhatsApp account being blocked from using WhatsApp, right now I'm very confused because my WhatsApp account has been blocked from using WhatsApp for no apparent reason because I didn't do anything at all.  which can violate whatsapp policy, I hope my whatsapp account can be removed from blocking whatsapp usage, because I don't feel that it violates whatsapp policy at all, thank you.  My whatsapp account is +6283151156627");
+    });
+    
+        document.getElementById("emailButton9").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","Halo pihak sekaligus developer whatsapp yang saya hormati, saya ingin menanyakan tentang perihal Akun whatsapp saya yang diblokir dari penggunaan whatsapp, saat ini saya sangat bingung karena Akun whatsapp saya diblokir dari penggunaan whatsapp tanpa sebab yang jelas karena saya sama sekali tidak melakukan hal-hal apapun yang dapat melanggar kebijakan whatsapp, Saya harap Akun whatsapp saya dapat dihapus dari pemblokiran penggunaan whatsapp, karena saya sama sekali tidak merasa melanggar kebijakan whatsapp, terimakasih. Akun whatsapp saya +6288212871196");
+    });
+    
+        document.getElementById("emailButton10").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","hello WhatsApp developer, I want to ask for my WhatsApp account to be restored because I need the WhatsApp account to contact my relatives and I also work at a company and I really need information from the WhatsApp group... my WhatsApp account is [+6288212871196]",['accessibility@support.whatsapp.com', 'support@support.whatsapp.com', 'smb@support.whatsapp.com', 'android@support.whatsapp.com', 'support@whatsapp.com']);
+    });
+    
+    document.getElementById("emailButton11").addEventListener("click", function() {
+      redirectToEmail("Bantuan WhatsApp Messenger","Halo selamat siang tim dukungan WhatsApp. maaf saya mengganggu kenyamanantim dukungan WhatsApp nomor whatsapp saya diblokir dan saya tidak dapat menggunakan Whatsapp. Saya meminta bantuan tim support Whatsapp untuk membuka blokir nomor WhatsApp saya. Saya sangat membutuhkan nomor whatsapp saya. dan maaf saya melanggar ketentuan layanan saya tidak melakukannya tahu perbuatanku merupakan pelanggaran sehingga nomor WhatsAppku diblokir. Saya mohon Tim dukungan WhatsApp untuk membuka blokir nomor saya: ");
+    });
+    
+    document.getElementById("emailButton12").addEventListener("click", function() {redirectToEmail("Bantuan WhatsApp Messenger", "HALO MARK ZUCKERBERG SAYA INGIN MENGAJUKAN PER TINJAUAN KARENA AKUN SAYA TERKENA BANNED PERMANEN, SAYA TAU PERMINTAAN SAYA AKAN DI TOLAK, TETAPI SAYA MEMERLUKAN NOMER ITU KARENA ADA BERKAS BERKAS KERJA DAN GROUP SEKOLAH ANAK SAYA , NOMER : [+62xx] , TOLONG SEGERA BUKA SECEPATNYA AGAR ANAK SAYA TIDAK DI MARAHIN GURU KARENA TELAT MENGERJAKAN TUGAS DI KARENAKAN NOMERNYA KE BLOCKIR");
+    });
